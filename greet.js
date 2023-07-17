fc1029d3e4794067cc27dddbb1b881f2256a4e0f
Saffah Khatib <skhatib1910@gmail.com>
2023-07-17 17:09:19 +0200

    intial commit

---
+++ greet.js
@@ -0,0 +1,12 @@
+function greet(name){
+    var result = "Hello, " + name;
+    return result;
+  };
+  
+  const msg = greet('Xola')
+  console.log(msg);
+
+  const person = greet('asisipho');
+  console.log(person);
+
+  console.log(greet('saffah'));
